a9ffd99e0b25a01c7c3d0e5f4dfe542a67692cb7
Samuel Benardo <sbenardo1@hwemail.com>
2025-09-19 15:00:38 -0700

    GP-2.1.1 Created Headers for Delete and Check Methods
    Also File Object Made

---
+++ DirectoriesAndFilesExistenceTester.java
@@ -0,0 +1,14 @@
+public class DirectoriesAndFilesExistenceTester {
+
+    public static verifyRepositoryInitialization() {
+        File verifier = new File();
+    }
+
+    public static cleanUp() {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}

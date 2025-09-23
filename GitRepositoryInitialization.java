1f50cd0895350e29ec05fca88965acc51b0d5aa8
Samuel Benardo <sbenardo1@hwemail.com>
2025-09-22 20:41:53 -0700

    GP-2.1.1 Settled on Program Format

---
+++ GitRepositoryInitialization.java
@@ -0,0 +1,20 @@
+public class GitRepositoryInitialization {
+
+    public static boolean needToCreateGitDirectory() {
+        File verifier = new File();
+    }
+
+    public static createGitDirectory() {
+        File creator = new File();
+    }
+
+    public static deleteGitDirectory() {
+
+    }
+
+    public static void main(String[] args) {
+        if (needToCreateGitDirectory()) {
+            createGitDirectory();
+        }
+    }
+}
--- gitExistenceVerification.java
+++
@@ -1,5 +0,0 @@
-public class gitExistenceVerification {
-    public static void main(String[] args) {
-        System.out.println("Git Repository Already Exists");
-    }
-}
